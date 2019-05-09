2e42910ef8bd079266d2b87e957cbe77b9c71c75
unknown <you@example.com>
2019-05-09 19:57:12 +0800

    Until calendar

---
+++ AngularCrud/src/app/employees/create-employee.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  department = '3';
+  isActive = false;
+  fullName  = '';
+  email = '';
+  phoneNumber = '';
+  contactPreference = 'email';
+  gender = 'male';
+  dateOfBirth = "";
+  departments: Department[] = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'},
+    {id: 4, name: 'Paroll'}
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+ saveEmployee(empForm: NgForm): void{
+   console.log(empForm);
+ };
+}
