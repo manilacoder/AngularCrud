640b0f1f38622850308319ccfb0836e9e964f0fd
unknown <you@example.com>
2019-05-10 12:46:22 +0800

    Model binded

--- AngularCrud/src/app/employees/create-employee.component.ts
+++ AngularCrud/src/app/employees/create-employee.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
 import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
+import {Employee} from '../models/employee.model'
 
 @Component({
   selector: 'app-create-employee',
@@ -9,17 +10,21 @@ import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-  datePickerConfig: Partial<BsDatepickerConfig>;
   previewPhoto = false;
-  photoPath = '';
-  department = '3';
-  isActive = false;
-  fullName  = '';
-  email = '';
-  phoneNumber = '';
-  contactPreference = 'email';
-  gender = 'male';
-  dateOfBirth: Date = new Date(2018,0,30);
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  
+  employee: Employee = {
+      id: null,
+      name: null,
+      gender: null,
+      contactPreference: null,
+      phoneNumber: null,
+      email: null,
+      dateOfBirth: null,
+      department: null,
+      isActive: null,
+      photoPath: null
+  };
   departments: Department[] = [
     {id: 1, name: 'Help Desk'},
     {id: 2, name: 'HR'},
@@ -29,9 +34,6 @@ export class CreateEmployeeComponent implements OnInit {
   constructor() { 
     this.datePickerConfig = Object.assign({}, {
       containerClass: 'theme-dark-blue', 
-      showWeekNumbers: false,
-      minDate: new Date(2018, 0, 1),
-      maxDate: new Date(2018, 11, 31),
       dateInputFormat: 'DD/MM/YYYY'
     });
   }
@@ -42,7 +44,7 @@ export class CreateEmployeeComponent implements OnInit {
 
   ngOnInit() {
   }
- saveEmployee(empForm: NgForm): void{
-   console.log(empForm);
+ saveEmployee(newEmployee: Employee): void{
+   console.log(newEmployee);
  };
 }
