4277cecca350ce67a885acca65d7508edc7344c2
unknown <you@example.com>
2019-05-10 12:01:39 +0800

    added error messages

--- AngularCrud/src/app/employees/create-employee.component.ts
+++ AngularCrud/src/app/employees/create-employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from '../models/department.model';
+import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -8,6 +9,9 @@ import { Department } from '../models/department.model';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  previewPhoto = false;
+  photoPath = '';
   department = '3';
   isActive = false;
   fullName  = '';
@@ -15,14 +19,26 @@ export class CreateEmployeeComponent implements OnInit {
   phoneNumber = '';
   contactPreference = 'email';
   gender = 'male';
-  dateOfBirth = "";
+  dateOfBirth: Date = new Date(2018,0,30);
   departments: Department[] = [
     {id: 1, name: 'Help Desk'},
     {id: 2, name: 'HR'},
     {id: 3, name: 'IT'},
     {id: 4, name: 'Paroll'}
   ];
-  constructor() { }
+  constructor() { 
+    this.datePickerConfig = Object.assign({}, {
+      containerClass: 'theme-dark-blue', 
+      showWeekNumbers: false,
+      minDate: new Date(2018, 0, 1),
+      maxDate: new Date(2018, 11, 31),
+      dateInputFormat: 'DD/MM/YYYY'
+    });
+  }
+  
+  togglePhotoPreview(){
+    this.previewPhoto = !this.previewPhoto;
+  }
 
   ngOnInit() {
   }
